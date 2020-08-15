475c8d08db582fd5ce74877e47ddd88eb5a8e584
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-08-16 04:48:00 +0900

    不要な箇所を削除

--- Assets/Scripts/Score/ScoreView.cs
+++ Assets/Scripts/Score/ScoreView.cs
@@ -6,52 +6,21 @@ namespace Score
 {
     public class ScoreView : MonoBehaviour
     {
-        [SerializeField]
-        private float punchTime = 0.5f;
-
-        [SerializeField]
-        private float punchScale = 1.5f;
-
-        private bool _isPunch = false;
-
-        private RectTransform _rectTransform;
-
         private TextMeshProUGUI _scoreText;
 
         private void Awake()
         {
-            _rectTransform = GetComponent<RectTransform>();
-
             _scoreText = GetComponent<TextMeshProUGUI>();
         }
 
         private void Start()
         {
-            _scoreText.text = "";
+            _scoreText.text = "Score:0";
         }
 
         public void ViewScoreText(int value)
         {
             _scoreText.text = "Score:"+ value;
-
-            //PunchText();
         }
-
-        // private void PunchText()
-        // {
-        //     //重複して呼ばれないように
-        //     if (_isPunch) return;
-        //     _isPunch = true;
-        //
-        //     transform.DOScale(_rectTransform.localScale * punchScale, punchTime / 2)
-        //         .OnComplete(() =>
-        //         {
-        //             transform.DOScale(_rectTransform.localScale / punchScale, punchTime / 2)
-        //                 .OnComplete(() =>
-        //                 {
-        //                     _isPunch = false;
-        //                 });
-        //         });
-        // }
     }
 }
