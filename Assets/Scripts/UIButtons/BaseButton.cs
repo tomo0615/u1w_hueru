21e68f1de0a267021895b85af225e4e102005fe4
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-08-16 16:17:28 +0900

    BaseButton,LoadButtonを実装

---
+++ Assets/Scripts/UIButtons/BaseButton.cs
@@ -0,0 +1,65 @@
+﻿using DG.Tweening;
+using Sound;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace UIButtons
+{
+    public class BaseButton : MonoBehaviour
+    {
+        [Inject] private AudioManager _audioManager = default;
+
+        private Button _button;
+
+        private const float ButtonAnimationTime = 0.1f;
+        private const float ScaleUpValue = 1.1f;
+
+        protected Button Button
+        {
+            get
+            {
+                if (_button == null)
+                {
+                    _button = GetComponent<Button>();
+                }
+
+                return _button;
+            }
+        }
+
+        private RectTransform _rectTransform;
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            Button.OnClickAsObservable()
+                .Subscribe(_ => { OnClicked(); })
+                .AddTo(gameObject);
+        }
+
+        public virtual void OnClicked()
+        {
+            PlayClickedSound();
+
+            DoPunchAnimation();
+        }
+
+        private void PlayClickedSound()
+        {
+            _audioManager.PlaySE(SEType.ButtonOK);
+        }
+
+        private void DoPunchAnimation()
+        {
+            DOTween.Sequence()
+                .Append(_rectTransform.DOScale(Vector3.one * ScaleUpValue, ButtonAnimationTime))
+                .Append(_rectTransform.DOScale(Vector3.one, ButtonAnimationTime));
+        }
+    }
+}
---
+++ Assets/Scripts/UIButtons/LoadButton.cs
@@ -0,0 +1,22 @@
+﻿using SceneLoader;
+using UnityEngine;
+using Zenject;
+
+namespace UIButtons
+{
+    public class LoadButton : BaseButton
+    {
+        [Inject]
+        private FadeSceneLoader _fadeSceneLoader = default;
+
+        [SerializeField]
+        private SceneName jumpSceneName = default;
+
+        public override void OnClicked()
+        {
+            base.OnClicked();
+
+            _fadeSceneLoader.JumpSceneLoad(jumpSceneName);
+        }
+    }
+}
