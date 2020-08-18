99705c2d5b8659e49cf38e86d38bdd2f1c60a7c1
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-08-18 13:49:36 +0900

    Tweet Buttonを実装

---
+++ Assets/Scripts/UIButtons/TweetButton.cs
@@ -0,0 +1,23 @@
+﻿using Score;
+using UnityEngine;
+using Zenject;
+
+namespace UIButtons
+{
+    public class TweetButton : BaseButton
+    {
+        [Inject] private ScorePresenter _scorePresenter;
+
+        [SerializeField] private string gameId = default;
+        
+        public override void OnClicked()
+        {
+            base.OnClicked();
+
+            var scoreValue = _scorePresenter.GetScoreValue();
+            
+            naichilab.UnityRoomTweet
+                .Tweet(gameId,"Score" + scoreValue + "を獲得しました！","unityRoom","unity1week");
+        }
+    }
+}
