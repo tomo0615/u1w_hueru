62b935370abe7efe41237ef2eddd4a6936475adb
tomonari <the.gamut.of.human.feelings@gmail.com>
2020-08-18 14:08:27 +0900

    Ranking Buttonを実装

---
+++ Assets/Scripts/UIButtons/RankingButton.cs
@@ -0,0 +1,18 @@
+﻿using Score;
+using Zenject;
+
+namespace UIButtons
+{
+    public class RankingButton : BaseButton
+    {
+        [Inject] private ScorePresenter _scorePresenter;
+        
+        public override void OnClicked()
+        {
+            base.OnClicked();
+
+            var scoreValue = _scorePresenter.GetScoreValue();
+            naichilab.RankingLoader.Instance.SendScoreAndShowRanking (scoreValue);
+        }
+    }
+}
